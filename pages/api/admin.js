4259951cf901d323614c036cbb87da9ddb065907
v13l <dev@ron.my.id>
2021-05-12 00:45:31 +0700

    feat(admin): add admin page
    add admin page for managing quote data

--- pages/api/admin.js
+++ pages/api/admin.js
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import { randomBytes } from 'crypto'
 import { withIronSession } from 'next-iron-session'
 
+// Supabase
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 
@@ -10,13 +11,51 @@ export default withIronSession(
     try {
       const user = req.session.get('user')
       if (!user) throw new Error('unauthorized')
+      const supabase = await createClient(supabaseUrl, supabaseKey)
 
-      res.send('login')
+      if (req.method === 'GET') {
+        const content = await supabase.from('ngobrolin_data')
+
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify({ data: content.data }))
+      }
+
+      if (req.method === 'DELETE') {
+        const val = req.body.val
+        await supabase.from('ngobrolin_data').delete().eq('id', val)
+
+        res.statusCode = 200
+        res.send('Deleted')
+      }
+
+      if (req.method === 'PATCH') {
+        const val = req.body.val
+        const id = req.body.id
+        await supabase
+          .from('ngobrolin_data')
+          .update({ quote: val })
+          .match({ id: id })
+        res.statusCode = 200
+        res.send('Update quote approved')
+      }
+
+      if (req.method === 'PUT') {
+        const val = req.body.val
+        const id = req.body.id
+
+        await supabase
+          .from('ngobrolin_data')
+          .update({ is_approved: val })
+          .match({ id: id })
+        res.statusCode = 200
+        res.send('Update is_approved')
+      }
     } catch (err) {
-      res.send('gagal')
+      res.send('failed connection to API ')
     }
   },
-  {
+ {
     cookieName: 'AUTH_COOKIE',
     cookieOptions: {
       secure: process.env.NODE_ENV === 'production' ? true : false
